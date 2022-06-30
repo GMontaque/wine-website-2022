5e4a82107396d7923e79e827ff25a65791881224
Giulio <62805702+knightrider909@users.noreply.github.com>
2022-06-30 18:56:51 +0100

    layout and jQuery update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,21 @@ $("#btn").on("click", function () {
 	$(".overlay").fadeOut();
 	$(".mainScreen").fadeIn();
 });
+
+$(".removeItem").on("click", function () {
+	// $(".shoppingCart-amount").html("&#8356; <span id='total'>0</span>");
+	let total = parseInt($("#total").html());
+	let price = parseInt($(this).attr("data-price"));
+	if (total > 0) {
+		total = total - price;
+		$("#total").html(total);
+	}
+});
+
+// #shop .product button
+$(".addItem").on("click", function () {
+	let price = parseInt($(this).attr("data-price"));
+	let total = parseInt($("#total").html());
+	total = total + price;
+	$("#total").html(total);
+});
