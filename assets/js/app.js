d457f236281ee307ade0b5696980212063211741
Giulio <62805702+knightrider909@users.noreply.github.com>
2022-07-02 17:12:25 +0100

    images added, style update, shop update

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,14 @@ $(".addItem").on("click", function () {
 	total = total + price;
 	$("#total").html(total);
 });
+
+// style
+$(function () {
+	$().timelinr({
+		orientation: "vertical",
+		issuesSpeed: 300,
+		datesSpeed: 100,
+		arrowKeys: "true",
+		startAt: 3,
+	});
+});
